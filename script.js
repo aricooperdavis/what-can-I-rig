dbe94e2a450eb60bccfbfd465c9e4c5f6966bba1
Ari Cooper-Davis <cooper-davis.arran@cooper-davis.net>
2022-03-09 09:50:50 +0000

    Refactoring

--- script.js
+++ script.js
@@ -43,67 +43,44 @@ goButton.onclick = function calculate() {
     ropes.push(el.firstChild.value);
   };
   ropes = ropes.map(el => parseInt(el)).filter(el => el).sort((a,b) => a<b);
+  let join_ropes = document.getElementById('join').checked;
+
+  // Reset output
+  tableData.innerHTML = '';
 
   // Get trip lengths
-  fetch('pitchlengths.txt')
-    .then(response => response.text())
-    .then(function(trips) {
-      // Parse and sort pitch lengths
-      trips = trips.split('\n').filter(
-        line => !(line.startsWith('#') | line.length < 3)
-      );
-      trips = trips.map(
-        line => line.split(',').map(
-          element => parseInt(element) ? parseInt(element) : element
-        )
-      );
-
-      // Filter trips based on lengths
-      valid_trips = filter_trips(trips, ropes).sort((a,b) => a[0]>b[0]);
-
-      // Reset and populate results table
-      tableData.innerHTML = '';
-      valid_trips.forEach(trip => {
-        let row = `<tr><td>${trip[0]}</td><td>${trip.slice(1).join(', ')}</td>`;
+  fetch('pitchlengths.txt').then(response => response.text()).then(function(trips) {
+    // Parse and sort pitch lengths
+    trips = trips.split('\n').filter(
+      line => !(line.startsWith('#') | line.length < 3)
+    );
+    for (let [_, trip] of Object.entries(trips)) {
+      trip = trip.split(',');
+      // Trips can't have more pitches than we have ropes
+      if (trip.length-1 > ropes.length) {
+        continue;
+      };
+      // Pitches must be possible
+      pitches = trip.slice(1).map(x => parseInt(x)).sort().reverse();
+      if (pitches.every((pitch, i) => {return pitch <= ropes[i]})) {
+        let row = `<tr><td>${trip[0]}</td><td>${trip.slice(1).join(', ')}</td></tr>`;
         tableData.insertAdjacentHTML('beforeend',row);
-      });
-
-      // Handle joins if selected
-      if (document.getElementById('join').checked) {
-        joined_trips = [];
-        join_combs(ropes).forEach(r => {
-          joined_trips = joined_trips.concat(filter_trips(trips, r));
-        });
-        joined_trips = Object.values(joined_trips.reduce((p,c) => (p[JSON.stringify(c)] = c,p),{}));
-
-        valid_trips = valid_trips.map(t => t[0])
-        joined_trips.sort((a,b) => a[0]>b[0]).forEach(trip => {
-          if (!valid_trips.includes(trip[0])) {
-            let row = `<tr class="join"><td>${trip[0]}</td><td>${trip.slice(1).join(', ')}</td>`;
+        continue;
+      };
+      if (join_ropes){
+        for (let [_, _join] of Object.entries(join_combs(ropes))) {
+          if (pitches.every((pitch, i) => {return pitch <= _join[i]})) {
+            let row = `<tr class="join"><td>${trip[0]}</td><td>${trip.slice(1).join(', ')}</td></tr>`;
             tableData.insertAdjacentHTML('beforeend',row);
-          }
-        });
-      }
-    });
-}
-
-// Filter possibe trips based on given ropes
-function filter_trips(trips, ropes) {
-  trips = trips.filter(pitches => {
-    // Shortcut: remove trips with more pitches than rope lengths
-    if (!(pitches.length-1 <= ropes.length)) {
-      return false;
+            break;
+          };
+        };
+      };
     };
-    // Compare pitch lengths and available rope lengths
-    return pitches.slice(0).sort((a,b) => a<b).slice(1).every((pitch, i) => {
-      return pitch <= ropes[i];
-    });
   });
+};
 
-  return trips;
-}
-
-// Get all rope length combinations possible by joining
+// Get all unique rope join combinations (reverse sorted)
 function join_combs(ropes) {
   function join(ropes) {
     let joins = [];
@@ -119,7 +96,7 @@ function join_combs(ropes) {
   joins.forEach(i => {
     if (i.length > 1) {
       join(i).forEach(j => {
-        joins.push(j);
+        joins.push(j.sort().reverse());
       });
     }
   });
